f60089601973910d4749023c8bdb552645f54c53
anilm <anilm@Ani>
2024-11-03 23:52:52 +0530

    initial proj01 sharing

---
+++ Gitproj01/src/com/nt/dao/Example.java
@@ -0,0 +1,12 @@
+package com.nt.dao;
+
+public class Example {
+	public void m1() {
+		System.out.println("Example.m1()  ===> for Learning" );
+		int a=10;
+		int b=20;
+		int c=a+b;
+		System.out.println(c);
+	}
+
+}
